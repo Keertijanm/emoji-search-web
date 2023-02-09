b30c17a87ae8922903dc31eed198234f525fd721
Keertijanm <81510729+Keertijanm@users.noreply.github.com>
2023-02-09 15:36:27 +0530

    Updates

---
+++ precache-manifest.42e833dad9bc77aa279e3677ead66cbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a5a708ca5198f5f9517ea0e58ab783",
+    "url": "/emoji-search-web/index.html"
+  },
+  {
+    "revision": "5260f9bd6e65d1be6d35",
+    "url": "/emoji-search-web/static/css/2.5510c45e.chunk.css"
+  },
+  {
+    "revision": "838ebbf865cbafb03634",
+    "url": "/emoji-search-web/static/css/main.65605343.chunk.css"
+  },
+  {
+    "revision": "5260f9bd6e65d1be6d35",
+    "url": "/emoji-search-web/static/js/2.b3fb42e2.chunk.js"
+  },
+  {
+    "revision": "6120b983a03909b7d2cd9bf134169811",
+    "url": "/emoji-search-web/static/js/2.b3fb42e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "838ebbf865cbafb03634",
+    "url": "/emoji-search-web/static/js/main.244de501.chunk.js"
+  },
+  {
+    "revision": "c6b7bd791633564657ab",
+    "url": "/emoji-search-web/static/js/runtime-main.8f6a6653.js"
+  }
+]);
